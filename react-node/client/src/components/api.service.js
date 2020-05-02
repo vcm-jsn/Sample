ea15eb9e32c8fdc2d35611843ab125b152a86b57
Anil Nayak <anil@hedgeloop.com>
2020-05-02 23:43:44 +0530

    updates secure post and secure fetch

--- react-node/client/src/components/api.service.js
+++ react-node/client/src/components/api.service.js
@@ -1,18 +1,29 @@
 import {API_HOST} from './config';
 import axios from 'axios';
 
-export function secureFetch(path){
-return axios.get(API_HOST + path);
-
+export function securefetch(url){
+	globalState.setData({isLoading: true})
+	return new Promise((resolve, reject)=>{
+		axios.get(API_HOST + url).then((response)=>{
+			globalState.setData({isLoading: false})
+			resolve(response)
+		}).catch((err)=>{
+			globalState.setData({isLoading: false})
+			reject(response)
+		})
+	})
 }
 
 export function securePost(url, body){
-
-    try{
-
-        return axios.post(API_HOST + url , body);
-    }catch(err){
-        console.error(err);
-    }
+	globalState.setData({isLoading: true})
+	return new Promise((resolve, reject)=>{
+		axios.post(API_HOST + url , body).then((response)=>{
+			globalState.setData({isLoading: false})
+			resolve(response)
+		}).catch((err)=>{
+			globalState.setData({isLoading: false})
+			reject(response)
+		})
+	})
 }
 
